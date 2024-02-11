3c748020fe49248f657376c04c550d18b8b4bb34
Nikita Rusetskii <rusetscky@outlook.com>
2024-02-11 22:06:51 +0100

    Add Solution to Chapter 2 Exercise 2 (#6)
    * Override the UserDetailsService component (2.3.1)
    
    * Override the endpoint authorization configuration (2.3.2)

---
+++ spring/security/security-demo/src/main/java/com/rusetskii/securitydemo/config/ProjectConfig.java
@@ -0,0 +1,43 @@
+package com.rusetskii.securitydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("john").password("12345").authorities("read").build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(it -> it.anyRequest().authenticated())
+                .build();
+    }
+}
