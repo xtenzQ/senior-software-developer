2aa6b560d44b1bec644a36c302366c47e8506b99
Nikita Rusetskii <rusetscky@outlook.com>
2024-07-07 16:01:29 +0200

    Add Answer Queries Problem

---
+++ algorithms-and-data-structures/src/test/java/com/xtenzq/arrays/PrefixSumTest.java
@@ -0,0 +1,48 @@
+package com.xtenzq.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static com.xtenzq.arrays.PrefixSum.answerQueries;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class PrefixSumTest {
+
+    @Test
+    void testAnswerQueries_BasicCase() {
+        int[] nums = {1, 6, 3, 2, 7, 2};
+        int[][] queries = {{0, 3}, {2, 5}, {2, 4}};
+        int limit = 13;
+        boolean[] expected = {true, false, true};
+        assertArrayEquals(expected, answerQueries(nums, queries, limit));
+    }
+
+    @Test
+    void testAnswerQueries_SingleElement() {
+        int[] nums = {5};
+        int[][] queries = {{0, 0}};
+        int limit = 10;
+        boolean[] expected = {true};
+        assertArrayEquals(expected, answerQueries(nums, queries, limit));
+    }
+
+    @Test
+    void testAnswerQueries_SingleQuery() {
+        int[] nums = {1, 2, 3, 4};
+        int[][] queries = {{1, 3}};
+        int limit = 10;
+        boolean[] expected = {true};
+        assertArrayEquals(expected, answerQueries(nums, queries, limit));
+    }
+
+    @Test
+    void testAnswerQueries_LargeQuery() {
+        int[] nums = new int[1000];
+        for (int i = 0; i < 1000; i++) {
+            nums[i] = i + 1;
+        }
+        int[][] queries = {{0, 999}, {0, 998}};
+        int limit = 500000;
+        boolean[] expected = {false, true};
+        assertArrayEquals(expected, answerQueries(nums, queries, limit));
+    }
+}
